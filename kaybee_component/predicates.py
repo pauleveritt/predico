b78905cf3717b11e8f8bcff7e7c1ee2e85ee4feb
Paul Everitt <pauleveritt@me.com>
2018-09-26 16:32:07 -0400

    Refactor predicates and predicate tests.

--- kaybee_component/predicates.py
+++ kaybee_component/predicates.py
@@ -1,15 +1,13 @@
 from dataclasses import dataclass
 from typing import Type, Any
 
-from kaybee_component.predicate_action import PredicateAction
+from kaybee_component.services.request.base_request import Request
 from kaybee_component.services.resource.base_resource import Resource
-from kaybee_component.services.view.base_view import IndexView
 
 
 @dataclass
 class Predicate:
-    action: PredicateAction
-    value: Type[Any]
+    value: Any
     key: str
     rank: int = 10
 
@@ -19,14 +17,19 @@ class Predicate:
             value = self.value  # Hope it's a string
         return f'{self.key}-{value}'
 
+    def matches(self, request: Request, **args) -> bool:
+        raise NotImplementedError
+
 
 @dataclass
 class ForPredicate(Predicate):
+    value: Type[Any]
     key: str = 'for_'
 
-    def matches(self, target: IndexView, request=None) -> bool:
-        # target and self.value are both classes. Are they the same?
-        return target is self.value
+    def matches(self, request: Request, **args) -> bool:
+        # for_ and self.value are both classes. Are they the same?
+        for_ = args['for_']
+        return for_ is self.value
 
 
 @dataclass
@@ -34,31 +37,29 @@ class ResourcePredicate(Predicate):
     value: Type[Resource]
     key: str = 'resource'
 
-    def matches(self, target: Resource, request=None) -> bool:
-        # target and self.value are both classes. Are they the same?
-        return target is self.value
+    def matches(self, request: Request, **args) -> bool:
+        resource_class = request.resource.__class__
+        return resource_class is self.value
 
 
 @dataclass
-class ParentSelfPredicate(Predicate):
-    """ Match on the resource id of yourself or a parent """
-
-    # This is useful when targeting a registration at a single resource,
-    # or a whole group of resources in part of a tree.
+class ResourceIdPredicate(Predicate):
+    """ Match on the resource id of yourself """
 
     value: str
-    key: str = 'parentself'
+    key: str = 'resourceid'
+    rank: int = 30
+
+    def matches(self, request: Request, **args) -> bool:
+        return self.value == request.resource.resourceid
+
+
+@dataclass
+class ParentIdPredicate(Predicate):
+    """ Match on the resource id of a parent """
+
+    value: str
+    key: str = 'parentid'
     rank: int = 20
-
-    def matches(self, target: str, request) -> bool:
-        # We are given an resourceid. Get the resource and look at
-        # it's resource id, then up the parents.
-
-        # TODO This does not work right. When there is a valid registration
-        # for both a parent and a child, it will match whichever happens
-        # to sort first. Child should be more precise than parent.
-        # Unfortunately that could mean that self.rank has to be
-        # dynamic which messes up sorted_actions being precomputed.
-
 
         return target == self.value
--- tests/unit/predicates/test_resourceid_predicate.py
+++ tests/unit/predicates/test_resourceid_predicate.py
@@ -1,10 +1,65 @@
-import dectate
+from dataclasses import dataclass
+from typing import List
+
 import pytest
-from dectate import ConflictError
 
-from kaybee_component.predicates import ResourcePredicate
-from kaybee_component.services.resource.base_resource import Resource
+from kaybee_component.predicates import ResourceIdPredicate
 
+
+@dataclass
+class TestResource:
+    parentids: List[str]
+    resourceid: str
+
+
+@dataclass
+class TestRequest:
+    resource: TestResource
+
+
+@pytest.fixture
+def test_resource():
+    resource = TestResource(
+        parentids=['more/index', 'index'],
+        resourceid='more/about'
+    )
+    return resource
+
+
+@pytest.fixture
+def test_request(test_resource):
+    tr = TestRequest(resource=test_resource)
+    return tr
+
+
+@pytest.fixture
+def resourceid_predicate():
+    pp = ResourceIdPredicate('more/about')
+    return pp
+
+
+@pytest.fixture
+def notmatches_resourceid_predicate():
+    pp = ResourceIdPredicate('xxx/yyy/zzz')
+    return pp
+
+
+def test_construction(resourceid_predicate):
+    assert 'more/about' == resourceid_predicate.value
+    assert 'resourceid' == resourceid_predicate.key
+    assert 30 == resourceid_predicate.rank
+
+
+def test_str(resourceid_predicate):
+    assert 'resourceid-more/about' == str(resourceid_predicate)
+
+
+def test_matches(resourceid_predicate, test_request):
+    assert resourceid_predicate.matches(test_request)
+
+
+def test_not_matches(notmatches_resourceid_predicate, test_request):
+    assert not notmatches_resourceid_predicate.matches(test_request)
 
 @pytest.fixture
 def committed_registry(registry, parentself_view):
