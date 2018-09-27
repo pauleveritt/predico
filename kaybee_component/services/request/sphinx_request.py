01894a88e55e22ab3b7aeeedafffd099c1d5999e
Paul Everitt <pauleveritt@me.com>
2018-09-27 14:28:33 -0400

    Implement request.views[SomeView]

--- kaybee_component/services/request/sphinx_request.py
+++ kaybee_component/services/request/sphinx_request.py
@@ -1,4 +1,6 @@
+from collections import UserDict
 from dataclasses import dataclass
+from typing import Any, Type
 
 from kaybee_component.registry import Registry
 from kaybee_component.servicemanager.manager import ServiceManager
@@ -25,8 +27,32 @@ class SphinxRequest:
         """ Given information in request, find best-match view """
 
         services = self.sm.services
-        view_service: ViewService = services['view']
-        return view_service.get_view(
+        viewservice: ViewService = services['view']
+        viewinstance = viewservice.get_view(
             self,
             for_=IndexView,
         )
+        return viewinstance
+
+    @property
+    def views(self):
+        """ Return a closure dict that can look up a view """
+
+        # We pack things up into a dict-like instance which
+        # has access to the view service and the request.
+        class ViewsGetter:
+            def __init__(self, viewservice: ViewService,
+                         request: SphinxRequest):
+                self.viewservice = viewservice
+                self.request = request
+
+            def __getitem__(self, viewclass: Type[Any]):
+                viewinstance = self.viewservice.get_view(
+                    self.request,
+                    for_=IndexView,
+                )
+                return viewinstance
+
+        viewservice: ViewService = self.sm.services['view']
+        viewsgetter = ViewsGetter(viewservice, self)
+        return viewsgetter
--- tests/unit/services/request/test_requestservice_makerequest.py
+++ tests/unit/services/request/test_requestservice_makerequest.py
@@ -13,6 +13,9 @@ def test_get_request_resource(requestservice, resourceservice, fake_article1):
     resourceservice.resources[fake_article1.id] = fake_article1
     request = requestservice.make_request('more/article1')
     assert 'more/article1' == request.resource.id
+    assert hasattr(request, 'resource')
+    assert hasattr(request, 'view')
+    assert hasattr(request, 'views')
 
 
 def test_get_request_view(fakearticle_view, requestservice, resourceservice,
@@ -20,3 +23,14 @@ def test_get_request_view(fakearticle_view, requestservice, resourceservice,
     resourceservice.resources[fake_article1.id] = fake_article1
     request = requestservice.make_request('more/article1')
     assert 'Fake Article View' == request.view.name
+
+
+def test_get_request_views(fakearticle_view, requestservice, resourceservice,
+                           fake_article1):
+    # Instead of going through the view property, access via the dict
+    # to get a differently-named view, e.g. request.views[OtherView].name
+    resourceservice.resources[fake_article1.id] = fake_article1
+    request = requestservice.make_request('more/article1')
+
+    view = request.views[fakearticle_view]
+    assert 'Fake Article View' == view.name
