e320af13dd2229822fa25f847ef9edc1e3b2ee62
Paul Everitt <pauleveritt@me.com>
2018-09-29 08:25:05 -0400

    pydantic now comes from the dataclass branch. Change base models to have validators to allow injectables and adapters to "validate".

--- predico/services/resource/base_resource.py
+++ predico/services/resource/base_resource.py
@@ -6,3 +6,13 @@ from dataclasses import dataclass
 class Resource:
     rtype: str
     id: str
+
+    # The following make pydantic happy
+    @classmethod
+    def validate(cls, v):
+        # do some validation or simply pass through the value
+        return v
+
+    @classmethod
+    def get_validators(cls):
+        yield cls.validate
